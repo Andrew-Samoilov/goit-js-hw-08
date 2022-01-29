dd7ce7a4d7722c3d912e13e95687280bd67a1f02
Andrew Samoilov <veprsag@gmail.com>
2022-01-29 13:30:39 +0200

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
 console.log(`6666`);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+function setVideoTime(){
+    let seco = localStorage.getItem("videoplayer-current-time", );
+    console.log(`On Page load ${seco}`);
+
+    player.setCurrentTime(seco).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+}
+
+setVideoTime();
+
+player.on('timeupdate', function() {
+    console.log('played the video!');
+    
+    player.getCurrentTime().then(function(seconds) {
+        // seconds = the current playback position
+        // console.log(`seconds ${seconds}`);
+        localStorage.setItem("videoplayer-current-time", seconds);
+        // localStorage.setItem("bosya-the-cat", 'super cool');
+        // localStorage.setItem("lord-the-dog", 'amazing');
+    }).catch(function(error) {
+        console.log(`seconds ${seconds}, error ${error}`);
+    });
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});

3562d82934d8198a448de80f8a9b399f449216bf
Andrew Samoilov <veprsag@gmail.com>
2022-01-30 17:59:30 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,15 +10,14 @@ const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onFormInput,500));
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 onPageLoad();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  console.log(`func onFormSubmit`);
-  console.log(`E-mail: ${formData.email}, Message: ${formData.message}`);
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(`E-mail: ${formData.email}, Message: ${formData.message}`);
 }
 
 function onFormInput(evt) {
@@ -28,9 +27,8 @@ function onFormInput(evt) {
 
 function onPageLoad() {
   console.log(`Loading..`);
-  const savedMess = localStorage.getItem(STORAGE_KEY);
-  const formText=JSON.parse(savedMess);
-  if (savedMess) {
+  const formText = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (formText) {
     refs.email.value = formText.email;
     refs.textarea.value = formText.message;
   }
